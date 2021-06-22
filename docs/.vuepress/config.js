a69dd6b3136833c8d036abb180ec705c257aa838
Kgothatso Ngako <kpangako@gmail.com>
2021-06-22 15:26:08 +0200

    Add head for mobile view override

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,4 +1,8 @@
 module.exports = {
+    head: [
+        ['meta', { charset: "UTF-8"}],
+        ['meta', { name: "viewport", content: "width=device-width, initial-scale=1.0"}]
+    ],
     dest: 'dist',
     plugins: ['check-md'],
     locales: {
