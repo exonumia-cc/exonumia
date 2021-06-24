c9c6bee1dccc480a50de97aa56425e0e7ec620c4
Kgothatso Ngako <kpangako@gmail.com>
2021-06-24 16:35:59 +0200

    Use logo as favicon

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,8 @@
 module.exports = {
     head: [
         ['meta', { charset: "UTF-8"}],
-        ['meta', { name: "viewport", content: "width=device-width, initial-scale=1.0"}]
+        ['meta', { name: "viewport", content: "width=device-width, initial-scale=1.0"}],
+        ['link', { rel: 'icon', href: '/exonumia-logo.png' }]
     ],
     dest: 'dist',
     plugins: ['check-md'],
